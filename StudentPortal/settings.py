4e4c51d8d97726a43dbe68b08bf652fcf75cd608
91H <rohit.barsila@hotmail.com>
2019-10-04 00:10:40 +0530

    Update settings.py

--- StudentPortal/settings.py
+++ StudentPortal/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+      'django.middleware.security.SecurityMiddleware',
+  'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,10 +120,8 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"), 
-]
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REST_FRAMEWORK={
     'DEFAULT_PERMISSION_CLASSES': (
