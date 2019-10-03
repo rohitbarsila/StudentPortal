b65e95827babecc0dc83be913cd95daf5bdd85fb
91H <rohit.barsila@hotmail.com>
2019-10-03 23:33:49 +0530

    Update settings.py

--- StudentPortal/settings.py
+++ StudentPortal/settings.py
@@ -121,14 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = "/static/"
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-)
-
 
 REST_FRAMEWORK={
     'DEFAULT_PERMISSION_CLASSES': (
